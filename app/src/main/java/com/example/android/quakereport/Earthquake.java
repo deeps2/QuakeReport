9ab28b99682ffb7da3f6b1992ef646d1fc49648d
SHIKHAR DEEP <deepshikhar24@yahoo.com>
2016-11-17 16:24:26 +0530

    changed UI, adden onItemClickListener

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -1,27 +1,33 @@
 package com.example.android.quakereport;
 
-/**
- * Created by Shikhar on 15-10-2016.
- */
 public class Earthquake {
 
-    private String mMagnitude, mLocation, mDate;
+    private String mMagnitude, mDate, mTime, mOffset, mPriLoc, mUrl;
 
-    public Earthquake(String magnitude, String location, String date){
+    public Earthquake(String magnitude, String Offset, String PriLoc, String date, String time, String url){
         mMagnitude = magnitude;
-        mLocation  = location;
         mDate      = date;
+        mTime      = time;
+        mOffset    = Offset;
+        mPriLoc    = PriLoc;
+        mUrl       = url;
     }
 
     public String getMagnitude(){
         return mMagnitude;
     }
 
-    public String getLocation(){
-        return mLocation;
-    }
-
     public String getDate(){
         return mDate;
     }
+
+    public String getTime(){
+        return mTime;
+    }
+
+    public String getOffset() { return mOffset; }
+
+    public String getPriLoc() { return  mPriLoc;}
+
+    public String getUrl() { return mUrl; }
 }
--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -1,6 +1,8 @@
 package com.example.android.quakereport;
 
 import android.content.Context;
+import android.graphics.drawable.GradientDrawable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,11 +11,65 @@ import android.widget.TextView;
 
 import java.util.List;
 
+import static com.example.android.quakereport.R.id.magnitude;
+
 
 public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
 
     public EarthquakeAdapter(Context context, List<Earthquake> earthquakes){
         super(context, 0, earthquakes);
+    }
+
+    private int getMagnitudeColor(String magnitude) {
+        int magnitudeColorResourceId;
+
+        double magnitudeValue = Double.parseDouble(magnitude);
+        int magnitudeFloor = (int) Math.floor(magnitudeValue);
+
+        switch (magnitudeFloor) {
+            case 0:
+            case 1:
+                magnitudeColorResourceId = R.color.magnitude1;
+                break;
+            case 2:
+                magnitudeColorResourceId = R.color.magnitude2;
+                break;
+            case 3:
+                magnitudeColorResourceId = R.color.magnitude3;
+                break;
+            case 4:
+                magnitudeColorResourceId = R.color.magnitude4;
+                break;
+            case 5:
+                magnitudeColorResourceId = R.color.magnitude5;
+                break;
+            case 6:
+                magnitudeColorResourceId = R.color.magnitude6;
+                break;
+            case 7:
+                magnitudeColorResourceId = R.color.magnitude7;
+                break;
+            case 8:
+                magnitudeColorResourceId = R.color.magnitude8;
+                break;
+            case 9:
+                magnitudeColorResourceId = R.color.magnitude9;
+                break;
+            default:
+                magnitudeColorResourceId = R.color.magnitude10plus;
+                break;
+        }
+        return ContextCompat.getColor(getContext(), magnitudeColorResourceId);
+        /*
+        Note on color values: In Java code, you can refer to the colors that you defined in the colors.xml
+        file using the color resource ID such as R.color.magnitude1, R.color.magnitude2. You still need to
+        convert the color resource ID into a color integer value though.
+
+        Example: int magnitude1Color = ContextCompat.getColor(getContext(), R.color.magnitude1);
+
+        You can call ContextCompat getColor() to convert the color resource ID into an actual integer
+        color value, and return the result as the return value of the getMagnitudeColor() helper method.
+        */
     }
 
     @Override
@@ -27,15 +83,34 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
 
         Earthquake currentEarthquake = getItem(position);
 
-        TextView magnitudeView = (TextView)listItemView.findViewById(R.id.magnitude);
+        TextView magnitudeView = (TextView)listItemView.findViewById(magnitude);
         magnitudeView.setText(currentEarthquake.getMagnitude());
 
-        TextView locationView = (TextView)listItemView.findViewById(R.id.location);
-        locationView.setText(currentEarthquake.getLocation());
+        // Set the proper background color on the magnitude circle.
+        // Fetch the background from the TextView, which is a GradientDrawable.
+        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeView.getBackground();
+
+        // Get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getMagnitudeColor(currentEarthquake.getMagnitude());
+
+        // Set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
+
+
+
+        TextView offsetView = (TextView)listItemView.findViewById(R.id.offset);
+        offsetView.setText(currentEarthquake.getOffset());
+
+        TextView priLocView = (TextView)listItemView.findViewById(R.id.primary_location);
+        priLocView.setText(currentEarthquake.getPriLoc());
 
         TextView dateView = (TextView)listItemView.findViewById(R.id.date);
         dateView.setText(currentEarthquake.getDate());
 
+        TextView timeView = (TextView)listItemView.findViewById(R.id.time);
+        timeView.setText(currentEarthquake.getTime());
+
         return listItemView;
     }
+
 }
