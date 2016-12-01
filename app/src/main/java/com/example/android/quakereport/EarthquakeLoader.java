164a3080e3ac90c73f1661d9df93241f10895522
SHIKHAR DEEP <deepshikhar24@yahoo.com>
2016-12-01 16:15:29 +0530

    first commit on loader branch

---
+++ app/src/main/java/com/example/android/quakereport/EarthquakeLoader.java
@@ -0,0 +1,43 @@
+package com.example.android.quakereport;
+
+import android.content.AsyncTaskLoader;
+import android.content.Context;
+import java.util.List;
+
+// Loads a list of earthquakes by using an AsyncTask to perform the network request to the given URL.
+
+public class EarthquakeLoader extends AsyncTaskLoader<List<Earthquake>> {
+
+    private static final String LOG_TAG = EarthquakeLoader.class.getName();
+
+    // Query URL
+    private String mUrl;
+
+    /**
+     * Constructs a new {@link EarthquakeLoader}.
+     *
+     * @param context of the activity
+     * @param url to load data from
+     */
+    public EarthquakeLoader(Context context, String url) {
+        super(context);
+        mUrl = url;
+    }
+
+    @Override
+    protected void onStartLoading() { //we also override the onStartLoading() method to call forceLoad() which is a required step to actually
+        forceLoad();                 // trigger the loadInBackground() method to execute.
+    }
+
+    // This is on a background thread.
+    @Override
+    public List<Earthquake> loadInBackground() {
+        if (mUrl == null) {
+            return null;
+        }
+
+        // Perform the network request, parse the response, and extract a list of earthquakes.
+        List<Earthquake> earthquakes = Utils.fetchEarthquakeData(mUrl);
+        return earthquakes;
+    }
+}
