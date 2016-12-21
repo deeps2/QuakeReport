eb79d8865e3f496140b3d6800714ba4dc23e4b35
SHIKHAR DEEP <deepshikhar24@yahoo.com>
2016-12-21 20:52:33 +0530

    overide onPreferenceChange()

--- app/src/main/java/com/example/android/quakereport/Settings.java
+++ app/src/main/java/com/example/android/quakereport/Settings.java
@@ -1,6 +1,9 @@
 package com.example.android.quakereport;
 
+import android.content.SharedPreferences;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,12 +15,42 @@ public class Settings extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
     }
 
-    public static class EarthquakePreferenceFragment extends PreferenceFragment {
+    public static class EarthquakePreferenceFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.settings_main);
+
+            /* we still need to update the preference summary when the settings activity is launched.
+               Given the key of a preference, we can use PreferenceFragment's findPreference() method to
+               get the Preference object, and setup the preference using a helper method called bindPreferenceSummaryToValue()
+            */
+            Preference minMagnitude = findPreference(getString(R.string.settings_min_magnitude_key));
+            bindPreferenceSummaryToValue(minMagnitude);
         }
+
+        //The code in this method takes care of updating the displayed preference summary after it has been changed.
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object value) {
+            String stringValue = value.toString();
+            preference.setSummary(stringValue);
+            return true;
+        }
+
+        /* bindPreferenceSummaryToValue() helper method to set the current EarhtquakePreferenceFragment
+           instance as the listener on each preference. We also read the current value of the preference
+           stored in the SharedPreferences on the device, and display that in the preference summary
+           (so that the user can see the current value of the preference).
+        */
+        private void bindPreferenceSummaryToValue(Preference preference) {
+
+            preference.setOnPreferenceChangeListener(this);
+
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(preference.getContext());
+            String preferenceString = preferences.getString(preference.getKey(), "");
+            onPreferenceChange(preference, preferenceString);
+        }
+
     }
 
 }
